949819d2f8b2a5d537364ed6fa9149054f32ff82
Hygor Costa <cost0142@algonquinlive.com>
2022-11-22 11:25:20 -0500

    add Toast fun

--- app/src/main/java/com/androidfinal_hygor_costa/MainActivity.kt
+++ app/src/main/java/com/androidfinal_hygor_costa/MainActivity.kt
@@ -2,10 +2,19 @@ package com.androidfinal_hygor_costa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+fun toast(message: String){
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
 }
