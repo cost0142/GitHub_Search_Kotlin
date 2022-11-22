239d1b4a7efc9f399a43e32a9846ceef030027aa
Hygor Costa <cost0142@algonquinlive.com>
2022-11-22 11:42:51 -0500

    add Ui

--- app/src/main/java/com/androidfinal_hygor_costa/MainActivity.kt
+++ app/src/main/java/com/androidfinal_hygor_costa/MainActivity.kt
@@ -4,17 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
 import android.widget.Toast
+import com.androidfinal_hygor_costa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var bilding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        bilding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bilding.root)
+
     }
 
 
-
-fun toast(message: String){
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
+    // region Toast Function
+    fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    // endregion
 
 }
