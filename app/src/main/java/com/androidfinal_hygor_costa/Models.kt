2c1136b46d654e474844dd6c3fed6eb8f536a5ee
Hygor Costa <cost0142@algonquinlive.com>
2022-11-25 12:54:54 -0500

    Json File

--- app/src/main/java/com/androidfinal_hygor_costa/Models.kt
+++ app/src/main/java/com/androidfinal_hygor_costa/Models.kt
@@ -2,6 +2,43 @@ package com.androidfinal_hygor_costa
 
 //Created by Hygor
 
+//region return JSon for one user
+
+// link https://api.github.com/search/users?q=android
+
+/*
+
+{
+total_count: 73824,
+incomplete_results: false,
+items: [
+{
+login: "android",
+id: 32689599,
+node_id: "MDEyOk9yZ2FuaXphdGlvbjMyNjg5NTk5",
+avatar_url: "https://avatars.githubusercontent.com/u/32689599?v=4",
+gravatar_id: "",
+url: "https://api.github.com/users/android",
+html_url: "https://github.com/android",
+followers_url: "https://api.github.com/users/android/followers",
+following_url: "https://api.github.com/users/android/following{/other_user}",
+gists_url: "https://api.github.com/users/android/gists{/gist_id}",
+starred_url: "https://api.github.com/users/android/starred{/owner}{/repo}",
+subscriptions_url: "https://api.github.com/users/android/subscriptions",
+organizations_url: "https://api.github.com/users/android/orgs",
+repos_url: "https://api.github.com/users/android/repos",
+events_url: "https://api.github.com/users/android/events{/privacy}",
+received_events_url: "https://api.github.com/users/android/received_events",
+type: "Organization",
+site_admin: false,
+score: 1
+}
+
+
+ */
+
+
+//endregion
 
 data class ResponseDataClass(
     val incomplete_results: Boolean = false,
