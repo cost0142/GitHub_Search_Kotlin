7bc0068379f1688746957d4bac292ebab680922e
Hygor Costa <cost0142@algonquinlive.com>
2022-12-03 22:26:25 -0500

    dec/02 add data_row and Results functios

--- app/src/main/java/com/androidfinal_hygor_costa/MainActivity.kt
+++ app/src/main/java/com/androidfinal_hygor_costa/MainActivity.kt
@@ -5,14 +5,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+import android.text.TextUtils
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.MotionEvent
+import android.view.View
 import android.view.accessibility.AccessibilityEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.androidfinal_hygor_costa.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,6 +56,67 @@ class MainActivity : AppCompatActivity() {
 
     private fun fetchJSONData() {
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val restApi = retrofit.create(RestApi::class.java)
+
+        if(TextUtils.isEmpty(binding.minFollowersEditText.text)) {
+            binding.minFollowersEditText.setText("0")
+
+        }
+
+        if(TextUtils.isEmpty(binding.minReposEditText.text)) {
+            binding.minReposEditText.setText("0")
+        }
+
+        val minNumberOfFollowers = binding.minFollowersEditText.text.toString().toInt()
+
+        val minNumbersOfRepos = binding.minReposEditText.text.toString().toInt()
+
+        val searchString = "${binding.searchUser.text} repos:>$minNumbersOfRepos followers:>=$minNumberOfFollowers"
+
+        val call = restApi.getUserData(searchString, binding.perPageNumberPicker.value)
+
+        binding.progressBar.visibility = View.VISIBLE
+
+        call.enqueue( object: Callback<ResponseDataClass>
+
+        {
+            override fun onResponse(
+                call: Call<ResponseDataClass>,
+                response: Response<ResponseDataClass>
+            ) {
+                val responseBody = response.body()
+
+                val users = responseBody?.items
+                val numberOfUsers = users?.size ?: 0
+
+                if(numberOfUsers > 0) {
+
+
+                    val intent = Intent(TheApp.context, ResultsActivity::class.java)
+
+                    intent.putParcelableArrayListExtra(getString(R.string.user_data_key), users)
+
+                    startActivity(intent)
+                }
+
+                binding.progressBar.visibility = View.GONE
+
+
+
+            }
+
+            override fun onFailure(call: Call<ResponseDataClass>, t: Throwable) {
+                toast(t.message.toString())
+                binding.progressBar.visibility = View.GONE
+            }
+
+
+        } )
     }
 
     // region: Options Menu
--- app/src/main/java/com/androidfinal_hygor_costa/ResultsActivity.kt
+++ app/src/main/java/com/androidfinal_hygor_costa/ResultsActivity.kt
@@ -1,11 +1,31 @@
 package com.androidfinal_hygor_costa
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androidfinal_hygor_costa.databinding.ActivityResultsBinding
 
 class ResultsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        binding = ActivityResultsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data: ArrayList<Users>?
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            data = intent.getParcelableArrayListExtra(getString(R.string.user_data_key), Users::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            data = intent.getParcelableArrayListExtra(getString(R.string.user_data_key))
+        }
+
+        supportActionBar?.title = "${data?.size} Results"
     }
+
 }
